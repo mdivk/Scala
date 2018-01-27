4d844926f751e277d90143bc8ec9cb64395db434
mdivk <xie3208080@gmail.com>
2018-01-27 09:38:04 -0500

    modified:   spark_read_and_join.scala

--- spark_read_and_join.scala
+++ spark_read_and_join.scala
@@ -132,12 +132,78 @@ res8: Long = 75408
 //final output: (order_id, (order_date, (order_item_product_id, order_item_subtotal))
 //daily revenue per product_id: (order_date, (order_item_product_id, order_item_subtotal))
 
-val ordersJoinMap = ordersJoin
+/* ordersJoin.first 
+res20: (Int, (String, (Int, Float))) = (65722,(2014-05-23,(365,119.98)))
+rec._2._1: 2014-05-23
+rec._2._2._1: 365
+rec._2._2._2: 119.98 
+
+What is needed for the output should be planned in advanced and then get the needed items from elements in the tuple through a map 
+
+*/
+
 val ordersJoinMap = ordersJoin.map(rec => ((rec._2._1, rec._2._2._1), rec._2._2._2))
+res18: ((String, Int), Float) = ((2014-05-23,365),119.98)
+val ordersJoinMap2 = ordersJoin.map(rec => (rec._2._1, (rec._2._2._1, rec._2._2._2)))
+res19: (String, (Int, Float)) = (2014-05-23,(365,119.98))
+
 ordersJoinMap.take(10).foreach(println)
 ordersJoinMap.count
-//((order_date, order_item_product_id), order_item_subtotal)
+
 val dailyRevenuePerProductId = ordersJoinMap.
   reduceByKey((revenue, order_item_subtotal) => revenue + order_item_subtotal)
 dailyRevenuePerProductId.take(10).foreach(println)
+((2013-11-04,775),29.97)
+((2013-08-17,235),34.99)
+((2014-04-06,793),44.97)
+((2014-01-15,365),7558.7393)
+((2013-09-24,792),14.99)
+((2014-07-12,728),260.0)
+((2014-07-11,565),210.0)
+((2013-12-11,116),179.96)
+((2014-04-22,627),1359.6599)
+((2014-07-14,957),4499.7)
+
+//sort by order_date 
+dailyRevenuePerProductId.take(10).sortBy(_._1._1).foreach(println)
+((2013-08-17,235),34.99)
+((2013-09-24,792),14.99)
+((2013-10-13,19),124.99)
+((2013-11-04,775),29.97)
+((2014-01-15,365),7558.7393)
+((2014-02-14,924),63.96)
+((2014-04-06,793),44.97)
+((2014-04-22,627),1359.6599)
+((2014-07-11,565),210.0)
+((2014-07-12,728),260.0)
+
+//sort by product_id
+dailyRevenuePerProductId.take(10).sortBy(_._1._2).foreach(println)
+((2013-12-11,116),179.96)
+((2013-08-17,235),34.99)
+((2014-01-15,365),7558.7393)
+((2014-07-11,565),210.0)
+((2014-04-22,627),1359.6599)
+((2014-07-12,728),260.0)
+((2013-11-04,775),29.97)
+((2013-09-24,792),14.99)
+((2014-04-06,793),44.97)
+((2014-07-14,957),4499.7)
+
+//sort by revenue in desc
+scala> dailyRevenuePerProductId.take(10).sortWith(_._2 > _._2).foreach(println)
+((2014-01-15,365),7558.7393)
+((2014-07-14,957),4499.7)
+((2014-04-22,627),1359.6599)
+((2014-07-12,728),260.0)
+((2014-07-11,565),210.0)
+((2013-12-11,116),179.96)
+((2014-04-06,793),44.97)
+((2013-08-17,235),34.99)
+((2013-11-04,775),29.97)
+((2013-09-24,792),14.99)
+
+
 dailyRevenuePerProductId.count
+res22: Long = 9120
+
