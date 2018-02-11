8b85e13ec6dbfeecfaf1c5e858ebae84dfc32039
mdivk <xie3208080@gmail.com>
2018-02-11 14:04:33 -0500

    modified:   96/spark -s51-81/s76.scala

--- 96/spark -s51-81/s76.scala
+++ 96/spark -s51-81/s76.scala
@@ -8,7 +8,7 @@ jdbc URL = jdbc:mysql://quickstart:3306/retail_db
 Columns ot order table : (order_id , order_date , order_customer_id,order_status) 
 Please accomplish following activities. 
 1. Copy "retail_db.orders" table to hdfs in a directory p91_orders. 
-2. Once data is copied to hdfs, using pyspark calculate the number of order for each status. 
+2. Once data is copied to hdfs, using scala calculate the number of order for each status. 
 3. use all the following methods to calculate the number of order for each status. (You need to know all these functions and its behavior for real exam) 
 - countByKey() 
 - groupByKey() 
@@ -35,7 +35,7 @@ mysql> select * from orders limit 10;
 +----------+---------------------+-------------------+-----------------+
 10 rows in set (0.00 sec)
 
-Step 1 : Import Single table 
+//Step 1 : Import Single table 
 sqoop import -m 1 \
 --connect jdbc:mysql://ms.itversity.com/retail_db \
 --username=retail_user \
@@ -43,7 +43,7 @@ sqoop import -m 1 \
 --table=orders \
 --target-dir=p91_orders  
 
-//Verify it:
+//Step 2: Verify it:
 hdfs dfs -cat p91_orders/part-m-00000
 68879,2014-07-09 00:00:00.0,778,COMPLETE
 68880,2014-07-13 00:00:00.0,1117,COMPLETE
@@ -51,24 +51,26 @@ hdfs dfs -cat p91_orders/part-m-00000
 68882,2014-07-22 00:00:00.0,10000,ON_HOLD
 68883,2014-07-23 00:00:00.0,5533,COMPLETE
 
+//Step 3 : countByKey 
+//#Number of orders by status 
+val allOrders = sc.textFile("p91_orders") 
+scala> allOrders.count
+res0: Long = 68883
 
-Note : Please check you dont have space between before or after '=' sign. 
-Sqoop uses the MapReduce framework to copy data from RDBMS to hdfs 
+scala> allOrders.first
+res1: String = 1,2013-07-25 00:00:00.0,11599,CLOSED
 
-Step 2 : Read the data from one of the partition, created using above command. 
-hadoop fs -cat p91_orders/part-m-00000
+//map it: <K, V> = <order_status, (order_id, order_date, order_customer_id, order_status)>
+val allOrdersMap = allOrders.map(rec => (rec.split(",")(3), (rec.split(",")(0), rec.split(",")(1).substring(0,10), rec.split(",")(2), rec.split(",")(3)) ))
+scala> allOrdersMap.first
+res10: (String, (String, String, String, String)) = (CLOSED,(1,2013-07-25,11599,CLOSED))
 
-Step 3 : countByKey 
-#Number of orders by status 
-allOrders = sc.textFile("p91_orders") 
-#Generate key and value pairs (key is order status and vale as an empty string 
-keyValue = allOrders.map(lambda line: (line.split(",”)[3] “”)) 
-#Using countByKey, aggregate data based on status as a key 
-output=keyValue.countByKey().items() 
-for line in output : print(line) 
+//Step 4 : groupByKey 
+scala> val groupedAllOrdersMap = allOrdersMap.groupByKey
+scala> groupedAllOrdersMap.count
+res0: Long = 9
 
-Step 4 : groupByKey 
-#Generate key and value pairs (key is order status and vale as an one 
+val filteredAllOrdersMap = groupedAllOrdersMap.filter(rec => (rec._1 == "CLOSED"))
 keyValue = allOrders.map(lambda line: (line.split)”,”)[3],1)) 
 #Using countByKey, aggregate data based on status as a key 
 output= keyValue.groupByKey().map(lambda kv: (kv[O], sum(kv[l]))) 
@@ -95,3 +97,28 @@ output=keyValue.combineByKey(lambda value: 1, lambda acc, value: acc+l, lambda a
 for line in output.collect() : print(line) 
 #Watch Spark Protessional Training provided by www.HadoopExam.com to understand more on each above functions. 
 (These are very important functions for real exam)
+
+
+//DO this with SparkSQL-----------------------------------------------------------------------
+scala> val allOrders = sc.textFile("p91_orders") 
+scala> allOrders.count
+res0: Long = 68883
+
+scala> val allOrdersMap = allOrders.map(rec =>(rec.split(",")(0), rec.split(",")(1).substring(0,10), rec.split(",")(2), rec.split(",")(3)))
+//| order_id | order_date          | order_customer_id | order_status
+scala> val allOrdersDF = allOrdersMap.toDF("order_id", "order_date", "order_customer_id", "order_status")
+scala> allOrdersDF.registerTempTable("orders")
+scala> val result = sqlContext.sql("select order_status, count(1) as Total from orders group by order_status")
++---------------+-----+
+|   order_status|Total|
++---------------+-----+
+|        PENDING| 7610|
+|        ON_HOLD| 3798|
+| PAYMENT_REVIEW|  729|
+|PENDING_PAYMENT|15030|
+|     PROCESSING| 8275|
+|         CLOSED| 7556|
+|       COMPLETE|22899|
+|       CANCELED| 1428|
+|SUSPECTED_FRAUD| 1558|
++---------------+-----+
