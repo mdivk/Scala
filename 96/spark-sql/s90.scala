0278e085fd533c766cc52799a08baf412a6990ec
mdivk <xie3208080@gmail.com>
2018-02-17 23:06:50 -0500

    modified:   96/spark-sql/s90.scala

--- 96/spark-sql/s90.scala
+++ 96/spark-sql/s90.scala
@@ -13,7 +13,7 @@ id,fee
 4,2900 
 
 Accomplish the following activities. 
-1. Select all the courses and their fees , whether tee is listed or not. 
+1. Select all the courses and their fees , whether fee is listed or not. 
 2. Select all the available fees and respective course. If course does not exists still list the fee 
 3. Select all the courses and their fees , whether tee is listed or not. However, ignore records having tee as null. 
 
@@ -35,12 +35,8 @@ fee.first()
 case class Course(id: Integer, name: String) 
 case class Fee(id: Integer, fee: Integer) 
 //create an RDD ot Product objects 
-val courseRDD = course.map(_.split(",")).map(c => 
-val feeRDD = , => Fee(c(O).tolnt,c(1).tolnt)) 
-courseRDD.first() 
-courseRDD.count() 
-feeRDD.first() 
-feeRDD.count() 
+val courseRDD = course.map(x=>(x.split(",")(0).toInt, x.split(",")(1)))
+val feeRDD = fee.map(x=> (x.split(",")(0).toInt, x.split(",")(1).toInt))
 
 // change RDD of Product objects to a DataFrame 
 val courseDF = courseRDD.toDF("id", "course") 
@@ -53,15 +49,52 @@ feeDF.registerTempTable("fee")
 // Select data from table 
 val results1 = sqlContext.sql( "SELECT * FROM course") 
 results1.show() 
++---+------+
+| id|course|
++---+------+
+|  1|Hadoop|
+|  2| Spark|
+|  3| HBase|
++---+------+
+
 
 val results2 = sqlContext.sql( "SELECT * FROM fee") 
 results2.show() 
++---+----+
+| id| fee|
++---+----+
+|  2|3900|
+|  3|4200|
+|  4|2900|
++---+----+
+
 
 val results3 = sqlContext.sql( "SELECT * FROM course LEFT JOIN fee ON course.id = fee.id") 
 results3.show() 
++---+------+----+----+
+| id|course|  id| fee|
++---+------+----+----+
+|  1|Hadoop|null|null|
+|  2| Spark|   2|3900|
+|  3| HBase|   3|4200|
++---+------+----+----+
 
 val results4 = sqlContext.sql("SELECT * FROM course RIGHT JOIN fee ON course.id = fee.id") 
 results4.show() 
++----+------+---+----+
+|  id|course| id| fee|
++----+------+---+----+
+|   2| Spark|  2|3900|
+|   3| HBase|  3|4200|
+|null|  null|  4|2900|
++----+------+---+----+
+
 
 val results5 = sqlContext.sql("SELECT * FROM course LEFT JOIN fee ON course.id = fee.id where fee.id IS NULL") 
 results5.show() 
++---+------+----+----+
+| id|course|  id| fee|
++---+------+----+----+
+|  1|Hadoop|null|null|
++---+------+----+----+
+
