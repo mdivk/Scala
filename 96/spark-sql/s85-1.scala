afa75788d47862b33ffd08c31642d747b2d54905
mdivk <xie3208080@gmail.com>
2018-03-10 08:17:57 -0500

    modified:   96/spark-sql/s85-1.scala

--- 96/spark-sql/s85-1.scala
+++ 96/spark-sql/s85-1.scala
@@ -58,30 +58,132 @@ results.show()
 
 
 Step 2 : Select code and name both separated by ' - ' and header name should be 'product Description'. 
-val results1 = sqlContext.sql("SELECT concat(productCode, name) as 'product Description', price FROM products") 
-
-org.apache.spark.sql.AnalysisException: cannot recognize input near 'as' ''product Description'' ',' in selection target; line 1 pos 41
-
-
-
-
+val results1 = sqlContext.sql("SELECT concat(productCode, name) as Product_Description, price FROM products") 
 results1.show() 
++--------------------+------+
+| Product_Description| price|
++--------------------+------+
+|2Quest Q64 10 FT....| 59.98|
+|2Under Armour Men...|129.99|
+|2Under Armour Men...| 89.99|
+|2Under Armour Men...| 89.99|
+|2Riddell Youth Re...|199.99|
+|2Jordan Men's VI ...|134.99|
+|2Schutt Youth Rec...| 99.99|
+|2Nike Men's Vapor...|129.99|
+|2Nike Adult Vapor...|  50.0|
+|2Under Armour Men...|129.99|
+|2Fitness Gear 300...|209.99|
+|2Under Armour Men...|139.99|
+|2Under Armour Men...| 89.99|
+|2Quik Shade Summi...|199.99|
+|2Under Armour Kid...| 59.99|
+|2Riddell Youth 36...|299.99|
+|2Under Armour Men...|129.99|
+|2Reebok Men's Ful...| 29.97|
+|2Nike Men's Finge...|124.99|
+|2Under Armour Men...|129.99|
++--------------------+------+
+only showing top 20 rows
+
 
 
 Step 3 : Select all distinct prices. 
 val results = sqlContext.sql("SELECT DISTINCT price AS Distinct_Price FROM products")
 results.show() 
++--------------+
+|Distinct_Price|
++--------------+
+|         108.0|
+|        179.98|
+|         12.99|
+|         99.98|
+|         29.99|
+|          25.0|
+|           8.0|
+|          72.0|
+|         241.0|
+|         63.99|
+|         59.98|
+|          50.0|
+|         169.0|
+|         219.0|
+|         194.0|
+|        139.99|
+|        189.99|
+|         81.99|
+|          10.8|
+|          75.0|
++--------------+
+only showing top 20 rows
+
+
 Step 4 : Select distinct price and name combination. 
 val results = sqlContext.sql(" SELECT DISTINCT price, name FROM products") 
 results.show() 
+
 Step 5 : Select all price data sorted by both code and productlD combination. 
 val results = sqlContext.sql("SELECT* FROM products ORDER BY code, productiD") 
 results.show() 
++------+--------------------+
+| price|                name|
++------+--------------------+
+| 24.99|Under Armour Men'...|
+|  79.0|lucy Women's Hath...|
+| 34.99|Cutters Adult C-T...|
+|  18.0|adidas Men's 2014...|
+|249.97|Easton Mako Youth...|
+|199.99|Nike Men's LeBron...|
+|  32.0|Nike Men's St. Lo...|
+| 19.99|Glove It Women's ...|
+| 19.99|Glove It Women's ...|
+|399.98|Field & Stream Sp...|
+| 34.97|Nike Men's Chicag...|
+|  26.0|Reebok Men's Toro...|
+| 29.99|Perfect Multi-Gym...|
+|179.99|Schutt XV Flex Sk...|
+|159.99|Rollerblade Men's...|
+| 21.99|Nike Men's Benass...|
+| 19.99|Glove It Women's ...|
+| 51.99|Titleist Pro V1 P...|
+|299.99|Nishiki Adult Mon...|
+|399.99|Teeter Hang-Ups E...|
++------+--------------------+
+only showing top 20 rows
+
+
 Step 6 : count number of products. 
 val results = sqlContext.sql("SELECT COUNT(1) AS total_count FROM products") 
 results.show() 
+
 Step 7 : Count number of products for each code. 
-val results = sqlContext.sql("SELECT code,COUNT(1) FROM products GROUP BY code ORDER BY count DESC") 
+
+val results = sqlContext.sql("SELECT productCode,COUNT(1) AS count FROM products GROUP BY productCode ORDER BY count DESC")
 results.show() 
-val results = sqlContext.sql("SELECT code,COUNT(1) AS count FROM products GROUP BY code ORDER BY count DESC")
-results.show() 
++-----------+-----+
+|productCode|count|
++-----------+-----+
+|         38|   48|
+|         41|   48|
+|         54|   24|
+|         31|   24|
+|         43|   24|
+|         37|   24|
+|         36|   24|
+|         40|   24|
+|         33|   24|
+|         52|   24|
+|         35|   24|
+|         34|   24|
+|         44|   24|
+|         32|   24|
+|         45|   24|
+|         39|   24|
+|         47|   24|
+|         46|   24|
+|         56|   24|
+|         48|   24|
++-----------+-----+
+only showing top 20 rows
+
+
