06e12c42cf67c3f223d1dd252decc8561e4fcd69
mdivk <xie3208080@gmail.com>
2018-03-09 23:07:28 -0500

    modified:   96/spark-sql/s90-1.scala

--- 96/spark-sql/s90-1.scala
+++ 96/spark-sql/s90-1.scala
@@ -3,51 +3,52 @@ Problem Scenario 90 : You have been given below two files
 course.txt 
 
 id,course 
-1,Hadoop 
-2, Spark 
-3,HBase 
+1,Hadoop
+2,Spark
+3,HBase
+5,Impala
 
 Fee.txt 
 id,fee 
-2, 3900 
-3,4200 
-4,2900 
+2,3900
+3,4200
+4,2900
+
 
 Accomplish the following activities. 
-1. Select all the courses and their fees , whether tee is listed or not. 
+1. Select all the courses and their fees , whether fee is listed or not. 
 2. Select all the available fees and respective course. If course does not exists still list the fee 
-3. Select all the courses and their fees , whether tee is listed or not. However, ignore records having tee as null. 
+3. Select all the courses and their fees , whether fee is listed or not. However, ignore records having fee as null. 
 
 Solution : 
 
 Step 1 . 
 hdfs dfs -mkdir sparksq14 
-hdfs dfs -put course.txt sparksq14/ 
-hdfs dfs -put tee.txt sparksq14/ 
+hdfs dfs -put course.txt sparksql4/ 
+hdfs dfs -put tee.txt sparksql4/ 
 
 Step 2 : Now in spark shell 
 
 //load the data into a new ROD 
-val course = sc.textFile("sparksq14/course.txt") 
-val fee = sc.textFile("sparksq14/fee.txt") 
-//Return the first element in this RDD 
+val course = sc.textFile("sparksql4/course.txt") 
+val fee = sc.textFile("sparksql4/fee.txt") 
 
-course.first() 
-fee.first() 
 
 //define the schema using a case class 
 case class Course(id: Integer, name: String) 
-case class Fee(id: Integer, fee: Integer) 
+scala> case class Fee(id: Int, amount: Float)
+
 
 //create an RDD ot Product objects 
-val courseRDD = course.map(_.split(",")).map(c => 
-val feeRDD = fee.map(Fee(c(0).tolnt,c(1).tolnt)) 
+val courseRDD = course.map(_.split(",")).map(x=>(Course(x(0).toInt, x(1))))
+val feeRDD = fee.map(_.split(",")).map(x=>(Fee(x(0).toInt, x(1).toFloat)))
+/*
 
 courseRDD.first() 
 courseRDD.count() 
 feeRDD.first() 
 feeRDD.count() 
-
+*/
 // change RDD of Product objects to a DataFrame 
 val courseDF = courseRDD.toDF() 
 val feeDF = feeRDD.toDF() 
@@ -56,16 +57,74 @@ val feeDF = feeRDD.toDF()
 courseDF.registerTempTable("course") 
 feeDF.registerTempTable("fee") 
 
-// Select data from table 
-val results = sqlContext.sql( "SELECT * FROM course") 
-results.show() 
-val results = sqlContext.sql( "SELECT * FROM fee") 
-results.show() 
 
-val results = sqlContext.sql("SELECT * FROM course LEFT JOIN fee ON course.id = fee.id") 
-results.show() 
-val results = sqlContext.sql("SELECT * FROM course RIGHT JOIN fee ON course.id = fee.id") 
-results.show() 
+/*course.txt 
+id,course 
+1,Hadoop
+2,Spark
+3,HBase
+5,Impala
 
-val results = sqlContext.sql("SELECT * FROM course LEFT JOIN fee ON course.id = fee.id where fee.id IS NULL") 
-results.show() 
+Fee.txt 
+id,amount 
+2,3900
+3,4200
+4,2900*/
+
+//1. Select all the courses and their fees , whether fee is listed or not. 
+sqlContext.sql("select c.id, c.course, isnull(f.amount, 'N/A') from course c left outer join fee f on f.id = c.id").show
+
+sqlContext.sql("select c.id, c.course, f.amount from course c left outer join fee f on f.id = c.id").show
++---+------+------+
+| id|course|amount|
++---+------+------+
+|  1|Hadoop|  null|
+|  2| Spark|3900.0|
+|  3| HBase|4200.0|
+|  5|Impala|  null|
++---+------+------+
+
+
+sqlContext.sql("select c.id, c.course, f.amount from course c join fee f on c.id = f.id").show
++---+------+------+
+| id|course|amount|
++---+------+------+
+|  2| Spark|3900.0|
+|  3| HBase|4200.0|
++---+------+------+
+
+
+sqlContext.sql("select * from course,fee").show
++---+------+---+------+
+| id|course| id|amount|
++---+------+---+------+
+|  2| Spark|  2|3900.0|
+|  3| HBase|  3|4200.0|
++---+------+---+------+
+
+sqlContext.sql("select * from course c right outer join fee f on f.id = c.id").show
++----+------+---+------+
+|  id|course| id|amount|
++----+------+---+------+
+|   2| Spark|  2|3900.0|
+|   3| HBase|  3|4200.0|
+|null|  null|  4|2900.0|
++----+------+---+------+
+
+
+sqlContext.sql("select * from course c left outer join fee f on f.id = c.id").show
++---+------+----+------+
+| id|course|  id|amount|
++---+------+----+------+
+|  1|Hadoop|null|  null|
+|  2| Spark|   2|3900.0|
+|  3| HBase|   3|4200.0|
+|  5|Impala|null|  null|
++---+------+----+------+
+
+
+//2. Select all the available fees and respective course. If course does not exists still list the fee 
+sqlContext.sql("").show
+
+//3. Select all the courses and their fees , whether fee is listed or not. However, ignore records having tee as null. 
+sqlContext.sql("").show
